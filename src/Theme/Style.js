b2a03949155242a792da348df8b1637c5c55687f
Yohanes Bandung <bandungpenting@gmail.com>
2020-03-09 03:55:03 +0700

    refactor(theme): use createGlobalStyle

--- src/Theme/Style.js
+++ src/Theme/Style.js
@@ -1,26 +1,20 @@
 // @flow
 import React from 'react';
+import {createGlobalStyle} from 'styled-components';
 import {colors} from './colors';
 
 type Props = {
   dark: boolean,
 };
 
-const Style = (props: Props) => (
-  <style
-    dangerouslySetInnerHTML={{
-      __html: `
-html {
-  background-color: ${props.dark ? colors.dark : colors.light};
-}
-::selection {
-  background-color: ${
-    props.dark ? colors.darkSelection : colors.lightSelection
-  };
-}
-      `,
-    }}
-  />
-);
+const Style = createGlobalStyle`
+  html {
+      background-color: ${props => (props.dark ? colors.dark : colors.light)};
+  }
+  &::selection {
+    background-color: ${props =>
+      props.dark ? colors.darkSelection : colors.lightSelection};
+  }
+`;
 
 export default React.memo<Props>(Style);
