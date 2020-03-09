fd0139cc8441de4d6ea482bed76a99fd587a897f
Yohanes Bandung <bandungpenting@gmail.com>
2020-03-10 02:42:35 +0700

    feature(Page): generate 404 Not Found page

--- src/Pages/NotFoundPage.js
+++ src/Pages/NotFoundPage.js
@@ -6,15 +6,26 @@ import Text from '../Components/Text';
 
 const NotFoundPage = () => {
   return (
-    <Text>
-      Contact:{' '}
-      <Link
-        href="mailto:bandungpenting@gmail.com?Subject=From%20ybbond.dev"
-        target="_top"
-      >
-        bandungpenting@gmail.com
-      </Link>
-    </Text>
+    <React.Fragment>
+      <Text>
+        <br />I regret to tell that you may have entered the
+      </Text>
+      <Text as="h2">404 page</Text>
+      <Text>
+        Website will typically generate a{' '}
+        <Text as="span" variant="red">
+          &quot;404 Not Found&quot;
+        </Text>{' '}
+        web page when a user attempts to follow a broken or dead link; hence the
+        404 error is one of the most recognizable errors encountered on the
+        World Wide Web. [
+        <Link to="https://en.wikipedia.org/wiki/HTTP_404">1</Link>]
+        <br />
+        <br />
+        <br />
+        get back to <Link to="/">safety »</Link>
+      </Text>
+    </React.Fragment>
   );
 };
 
