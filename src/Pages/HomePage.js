4232a5f6c0421262f49afc6f5493511d839e7c58
Yohanes Bandung <bandungpenting@gmail.com>
2020-03-09 21:31:49 +0700

    feature(page): add default HomePage

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,41 @@
+// @flow
+import React from 'react';
+
+import Link from '../Components/Link';
+import Text from '../Components/Text';
+
+const CVPage = () => {
+  return (
+    <React.Fragment>
+      <Text>
+        Currently work as Frontend Engineer of kumparan.com in Jakarta,
+        Indonesia. Building it with React, React Native for Android and iOS.
+        <br />
+        <br />
+        Eager to learn.
+        <br />
+        Hardships.
+        <br />
+        I cannot say that I am humble, but I learn from experience that each
+        individuals, their own excellences.
+        <br />
+        I try to learn from everyone, and everything.
+        <br />
+        <br />
+        Contact:{' '}
+        <Link
+          to="mailto:bandungpenting@gmail.com?Subject=From%20ybbond.dev"
+          target="_top"
+        >
+          bandungpenting@gmail.com
+        </Link>
+      </Text>
+      <Text>
+        Other site:{' '}
+        <Link to="https://reason.ybbond.dev">reason.ybbond.dev</Link>
+      </Text>
+    </React.Fragment>
+  );
+};
+
+export default React.memo<{}>(CVPage);
