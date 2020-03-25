523ba2cf900307dbefc595cf4346446ca0323790
Yohanes Bandung <bandungpenting@gmail.com>
2020-03-25 23:43:19 +0700

    refactor(webpack): add analyzer, preparing for dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+require('dotenv').config();
 
-module.exports = {
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+const config = {
   entry: path.resolve(__dirname, 'src/index'),
   module: {
     rules: [
@@ -18,6 +24,12 @@ module.exports = {
       template: 'public/index.html',
       favicon: 'public/favicon.ico',
     }),
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.GITHUB_READ_ONLY_TOKEN': JSON.stringify(
+        process.env.GITHUB_READ_ONLY_TOKEN,
+      ),
+    }),
   ],
   devServer: {
     contentBase: path.resolve(__dirname, 'build'),
@@ -25,8 +37,19 @@ module.exports = {
     hot: true,
     port: 3000,
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
   },
 };
+
+if (process.env.WEBPACK_ANALYZE_WEB === 'true') {
+  config.plugins.push(new BundleAnalyzerPlugin());
+}
+
+module.exports = config;
